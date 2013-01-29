29626f4e7357b2eff5af8b523680bb2fbed37f1c
Dennis Crissman <dcrissma@redhat.com>
2013-01-29 12:43:59 -0500

    add licensing

--- cacher-core/src/main/java/cacher/fetcher/FetchMultiple.java
+++ cacher-core/src/main/java/cacher/fetcher/FetchMultiple.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ * Author: Dennis Crissman
+ *
+ * Licensed under the GNU Lesser General Public License, version 3 or
+ * any later version.
+ *
+ * In addition to the conditions of LGPLv3, you must preserve author
+ * attributions in source code distributions and must accompany binary
+ * distributions with such author attributions.
+ */
+
 package cacher.fetcher;
 
 import java.util.List;
--- cacher-memcached/src/test/java/cacher/memcached/TestMemcachedCache.java
+++ cacher-memcached/src/test/java/cacher/memcached/TestMemcachedCache.java
@@ -1,10 +1,20 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ * Author: Dennis Crissman
+ *
+ * Licensed under the GNU Lesser General Public License, version 3 or
+ * any later version.
+ *
+ * In addition to the conditions of LGPLv3, you must preserve author
+ * attributions in source code distributions and must accompany binary
+ * distributions with such author attributions.
+ */
+
 package cacher.memcached;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import cacher.memcached.MemcachedCache;
 
 public class TestMemcachedCache {
 
